65f0e0910feeef69cd2cd385a098605fedf8b1c0
조병근 <airmancho@korea.ac.kr>
2022-08-23 16:28:45 +0900

    🐛: timezone 에러 수정

--- src/alarm/alarm.service.ts
+++ src/alarm/alarm.service.ts
@@ -186,9 +186,7 @@ export class AlarmService {
             const date = new Date().getDate();
             const month = new Date().getMonth();
             const year = new Date().getFullYear();
-            // new Date(year, month, date, hour, minute, 0)
-            const job = new CronJob(new Date(year, month, date, hour+12, minute, 0), async () => {
-                console.log('Sended!')
+            const job = new CronJob(new Date(year, month, date, hour - 9, minute, 0), async () => {
                 await this.pushNotiService.sendPush(
                     antoherMemberDataForMe.id,
                     anotherMemberProfile.User.device_token,
@@ -211,8 +209,6 @@ export class AlarmService {
                 );
             });
             this.schedulerRegistry.addCronJob(`alarm-${alarm.id}`, job);
-            console.log(new Date(year, month, date, hour+9, minute, 0));
-            console.log('schedule registered!')
             job.start();
             await queryRunner.commitTransaction();
         } catch(e) {
