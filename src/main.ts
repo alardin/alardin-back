4b9f7c6d9473cd0ea6c64d7eb897148295975c9a
조병근 <airmancho@korea.ac.kr>
2022-09-29 00:15:22 +0900

    ✨: 서버 상태 노션 전달

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,56 @@ import * as passport from 'passport';
 import { UndefinedToNullInterceptor } from './common/interceptors/undefined-to-null.interceptor';
 import { utilities, WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
+import { Client } from "@notionhq/client";
+
+const notion = new Client({ auth: process.env.NOTION_KEY });
+const databaseId = process.env.NOTION_SERVER_DB_ID;
+
+async function updateNotion(status: string, message?: string) {
+  const now = new Date();
+  let years = now.getFullYear();
+  let months: string | number = now.getMonth()+1;
+  let date = now.getDate();
+  let hours: string | number = now.getHours();
+  let minutes: string | number = now.getMinutes();
+  months = months <= 9 ? `0${months}` : months;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  hours = hours < 10 ?  `0${hours}` : hours;
+  await notion.pages.create({
+    parent: { database_id: databaseId },
+    properties: {
+      Name: {
+        type: 'title',
+        title: [{
+            type: 'text',
+            text: {
+                content: `check_${months}/${date}/${years} ${hours}:${minutes}`
+            }
+        }]
+      },
+      Tags: {
+          type: 'multi_select',
+          multi_select: [
+              {
+                  name: status === "ALIVE" ? status : "DEAD",
+                  color: status === "ALIVE" ? 'green' : 'red'
+              }
+          ]
+      },
+      Message: {
+        type: 'rich_text',
+        rich_text: [
+            {
+                text: {
+                    content: message
+                },
+            }
+        ]
+      },
+    }
+  });
+}
+
 
 async function bootstrap() {
   const port = +process.env.PORT || 3030;
@@ -37,9 +87,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(port, '0.0.0.0',  () => {
+  await app.listen(port, '0.0.0.0',  async () => {
     console.log(`[*] listening on ${port}!`);
-  });
+    await updateNotion("ALIVE", "SUCCESS");
+  }).catch(async e => { await updateNotion('DEAD', e.message) });
 }
 // test
 bootstrap();
