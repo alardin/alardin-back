eb38e87decf68d3a10c86b57dbb60d1d0e9b820c
조병근 <airmancho@korea.ac.kr>
2022-09-29 01:40:57 +0900

    🐛: dto 컬럼 삭제

--- src/game/dto/save-game.dto.ts
+++ src/game/dto/save-game.dto.ts
@@ -1,6 +1,6 @@
 import { PickType } from "@nestjs/swagger";
 import { AlarmResults } from "src/entities/alarm.results.entity";
 
-export class SaveGameDto extends PickType(AlarmResults, ['start_time', 'end_time', 'play_time', 'trial', 'Game_id', 'Game_channel_id', 'Alarm_id', 'is_bot_used', 'is_cleared']) {
+export class SaveGameDto extends PickType(AlarmResults, ['start_time', 'end_time', 'Game_id', 'Game_channel_id', 'Alarm_id', 'is_cleared']) {
 
 }
