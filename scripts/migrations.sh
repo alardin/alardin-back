95df5f1f806838cc1a35db3799571ff9a5db3e21
조병근 <airmancho@korea.ac.kr>
2022-09-30 00:53:35 +0900

    test

--- scripts/migrations.sh
+++ scripts/migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "[*] RUN Migrate!!"
+echo "[*] RUN Migrate!"
 cd /home/ec2-user/alardin-back-migrations
 cp ../.alardin-envs/.migration.env .env && npm install && npm run migrate:run \
     && cd .. && rm -rf alardin-back-migrations
