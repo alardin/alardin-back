5e9e4b35827e806f198c74fdfdd8cee7baa0daa8
조병근 <airmancho@korea.ac.kr>
2022-09-29 23:43:42 +0900

    run.sh 수정

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,6 @@ echo "[*] RUN SERVER by CodeDeploy!"
 cd /home/ec2-user/alardin-back-test
 
 docker system prune -f
-docker-compose down
+docker-compose -f docker-compose/docker-compose.server.yml down
 docker rmi $(docker images -aq)
 docker-compose up -d --build api-prod
