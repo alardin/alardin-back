d49b9c265fc1ae6c7b1a481dea6baddce2b922c1
조병근 <airmancho@korea.ac.kr>
2022-09-30 00:33:07 +0900

    yml 경로 수정

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,13 @@
 echo "[*] RUN SERVER by CodeDeploy!!"
 cd /home/ec2-user/alardin-back-test
 
+images=`docker images -aq`
+compose-file=""
+
 docker system prune -f
 docker-compose -f docker-compose/docker-compose.server.yml down
-docker rmi $(docker images -aq)
+if [ ! -n $images ]
+then
+    docker rmi "$images"
+fi
 docker-compose up -f docker-compose/docker-compose.server.yml -d --build api-prod
