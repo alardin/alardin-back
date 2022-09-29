df423a8554e9358c5f4222759713723ce37e45bd
조병근 <airmancho@korea.ac.kr>
2022-09-29 15:14:22 +0900

    ✨: 서버 시작 스크립트에

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,11 @@
 
 echo "[*] RUN SERVER by CodeDeploy!"
 cd /home/ec2-user/alardin-back-test
+
+echo "[*] RUN Migrate"
+npm install && npm run migrate:run
+rm -rf node_modules
+
 docker system prune -f
 docker-compose down
 docker rmi $(docker images -aq)
