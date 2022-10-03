5fc6b0193438d60d19a1768b31b4585f9e4c779a
조병근 <airmancho@korea.ac.kr>
2022-10-03 13:11:38 +0900

    docker pull로 수정

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,9 @@ echo "[*] RUN SERVER by CodeDeploy!!"
 cd /home/ec2-user/alardin-back-test
 
 images=`docker images -aq`
+export DH_USERNAME=`aws ssm get-parameters --name DOCKERHUB_USERNAME --query Parameters[0].Value | sed s'/"//g'`
+export DH_TOKEN=`aws ssm get-parameters --name DOCKERHUB_TOKEN --query Parameters[0].Value | sed s'/"//g'`
+docker login -u $DH_USERNAME -p $DH_TOKEN
 
 docker system prune -f
 docker-compose -f docker-compose/docker-compose.server.yml down
@@ -11,4 +14,6 @@ if [ ! -n $images ]
 then
     docker rmi "$images"
 fi
-docker-compose -f docker-compose/docker-compose.server.yml up -d --build api-prod
+docker-compose -f docker-compose/docker-compose.server.yml pull api-prod && \
+    docker-compose -f docker-compose/docker-compose.server.yml start -d api-prod
+# docker-compose -f docker-compose/docker-compose.server.yml up -d --build api-prod
