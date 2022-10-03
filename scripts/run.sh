27ccd971444ab85fa9439d387e361ea766c94bf7
조병근 <airmancho@korea.ac.kr>
2022-10-03 21:45:38 +0900

    🐛: run.sh 변수 추가

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,9 +6,9 @@ cd /home/ec2-user/alardin-back-test
 images=`docker images -aq`
 export DH_USERNAME=`aws ssm get-parameters --name DOCKERHUB_USERNAME --query Parameters[0].Value | sed s'/"//g'`
 export DH_TOKEN=`aws ssm get-parameters --name DOCKERHUB_TOKEN --query Parameters[0].Value | sed s'/"//g'`
-export DOCKER_REGISTRY=`aws ssm get-parameters --name DOCKERHUB_USERNAME --query Parameters[0].Value | sed s'/"//g'`
-export DOCKER_APP_NAME=`aws ssm get-parameters --name DOCKERHUB_TOKEN --query Parameters[0].Value | sed s'/"//g'`
-export IMAGE_TAG=`aws ssm get-parameters --name DOCKERHUB_USERNAME --query Parameters[0].Value | sed s'/"//g'`
+export DOCKER_REGISTRY=`aws ssm get-parameters --name DOCKER_REGISTRY --query Parameters[0].Value | sed s'/"//g'`
+export DOCKER_APP_NAME=`aws ssm get-parameters --name DOCKER_APP_NAME --query Parameters[0].Value | sed s'/"//g'`
+export IMAGE_TAG=`aws ssm get-parameters --name IMAGE_TAG --query Parameters[0].Value | sed s'/"//g'`
 docker login -u $DH_USERNAME -p $DH_TOKEN
 
 docker system prune -f
