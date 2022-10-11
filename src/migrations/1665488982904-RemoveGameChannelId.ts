05e41661025f1a8f021eb3d1aecba8f0de589014
이상혁 <desummit1@gmail.com>
2022-10-11 20:53:51 +0900

    test

--- src/migrations/1665488982904-RemoveGameChannelId.ts
+++ src/migrations/1665488982904-RemoveGameChannelId.ts
@@ -6,7 +6,7 @@ export class RemoveGameChannelId1665488982904 implements MigrationInterface {
       `ALTER TABLE alarm_results DROP FOREIGN KEY FK_8e30edd9c346d13f3a2efe2fd34`,
     );
     await queryRunner.query(
-      `ALTER TABLE alarm_results DROP COLUMN Game_channel_id;`,
+      `ALTER TABLE alarm_results DROP COLUMN Game_channel_id;gi`,
     );
   }
 
