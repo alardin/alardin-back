6fe59d7e6d3168c9bdcb502e3d54f19263dc739e
조병근 <airmancho@korea.ac.kr>
2022-10-12 23:42:05 +0900

    test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Public } from 'src/common/decorators/public.decorator';
 import { AuthDto } from 'src/users/dto/auth.dto';
 import { AuthService } from './auth.service';
 import { AppleLoginDto } from './dto/apple-login.dto';
@@ -22,6 +23,7 @@ export class AuthController {
         return await this.authService.kakaoAuth(tokens);
     }
 
+    @Public()
     @Post('apple')
     async appleAuth(@Body() data: AppleLoginDto) {
         await this.authService.appleAuth(data, data.deviceToken);
