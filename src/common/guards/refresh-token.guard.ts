0e5f122a7a8fa3d6524fdf74f32bec1c6b28ea0e
조병근 <airmancho@korea.ac.kr>
2022-10-13 22:12:20 +0900

    test

--- src/common/guards/refresh-token.guard.ts
+++ src/common/guards/refresh-token.guard.ts
@@ -1,11 +1,13 @@
 import { ExecutionContext, Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { Request } from "express";
 import { Observable } from "rxjs";
 
 @Injectable()
 export class RefreshTokenGuard extends AuthGuard('refreshToken') {
 
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        return super.canActivate(context);
+        const request: Request = context.switchToHttp().getRequest();
+        return request.isAuthenticated();
     }
 }
