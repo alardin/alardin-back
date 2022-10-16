7ddb4f42e63925aa3916c42e35002971dd551762
조병근 <airmancho@korea.ac.kr>
2022-10-16 16:38:52 +0900

    test

--- src/migrations/1665905689147-AddIsPrivateColumnToUsers.ts
+++ src/migrations/1665905689147-AddIsPrivateColumnToUsers.ts
@@ -1,14 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class GameTableClear1665902833317 implements MigrationInterface {
+export class AddIsPrivateColumnToUsers1665905689147 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `DELETE FROM games;`
-        );
-        await queryRunner.query(
-            `ALTER TABLE games AUTO_INCREMENT = 1`
-        );
+            `ALTER TABLE users ADD COLUMN is_private boolean DEFAULT false`
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
