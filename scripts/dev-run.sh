81584631e4b923b0561015ec16e27bb5cb9668f2
조병근 <airmancho@korea.ac.kr>
2022-10-17 20:00:10 +0900

    edit environment variables

--- scripts/dev-run.sh
+++ scripts/dev-run.sh
@@ -2,7 +2,7 @@
 
 echo "[*] RUN SERVER by CodeDeploy!"
 cd /home/ec2-user/alardin-back-test
-
+cp ../.alardin-envs/.dev.env .env
 images=`docker images -aq`
 export DH_USERNAME=`aws ssm get-parameters --name DOCKERHUB_USERNAME --query Parameters[0].Value | sed s'/"//g'`
 export DH_TOKEN=`aws ssm get-parameters --name DOCKERHUB_TOKEN --query Parameters[0].Value | sed s'/"//g'`
