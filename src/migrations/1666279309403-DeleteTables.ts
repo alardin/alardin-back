6f13213ffaa747fe3cc4d3085dd3ea10a77785f9
조병근 <airmancho@korea.ac.kr>
2022-10-21 00:26:48 +0900

    Delete Tables

--- src/migrations/1666279309403-DeleteTables.ts
+++ src/migrations/1666279309403-DeleteTables.ts
@@ -4,7 +4,10 @@ export class DeleteTables1666279309403 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `DROP table`
+            `ALTER TABLE game_used_images DROP FOREIGN KEY FK_ea24c8afa538981f66a84119dcd;`
+        );
+        await queryRunner.query(
+            `DROP TABLE IF EXISTS game_play_images, game_play_keywords, game_used_images`
         );
     }
 
