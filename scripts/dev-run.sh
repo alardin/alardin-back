3b812de6c3a19413b4d09a4dddd99382de2dfe6c
조병근 <airmancho@korea.ac.kr>
2022-10-22 23:55:42 +0900

    Add Env variables

--- scripts/dev-run.sh
+++ scripts/dev-run.sh
@@ -5,10 +5,12 @@ cd /home/ec2-user/alardin-back-test
 cp ../.alardin-envs/.dev.env .env
 images=`docker images -aq`
 export DH_USERNAME=`aws ssm get-parameters --name DOCKERHUB_USERNAME --query Parameters[0].Value | sed s'/"//g'`
+export DH_USERNAME=`aws ssm get-parameters --name DOCKERHUB_USERNAME --query Parameters[0].Value | sed s'/"//g'`
 export DH_TOKEN=`aws ssm get-parameters --name DOCKERHUB_TOKEN --query Parameters[0].Value | sed s'/"//g'`
 export DOCKER_REGISTRY=`aws ssm get-parameters --name DOCKER_REGISTRY --query Parameters[0].Value | sed s'/"//g'`
 export DOCKER_APP_NAME=`aws ssm get-parameters --name DOCKER_APP_NAME --query Parameters[0].Value | sed s'/"//g'`
 export IMAGE_TAG=`aws ssm get-parameters --name IMAGE_TAG --query Parameters[0].Value | sed s'/"//g'`
+export GOOGLE_APPLICATION_CREDENTIALS='/home/ec2-user/alardin-back-test/firebase/service-account.json'
 docker login -u $DH_USERNAME -p $DH_TOKEN
 
 docker system prune -f
