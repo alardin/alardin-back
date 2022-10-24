6f048d6c2ed1255efeaec46407b69a3a2aa17684
조병근 <airmancho@korea.ac.kr>
2022-10-24 23:02:02 +0900

    Migrate: apple null

--- src/migrations/1666619963285-AppleNull.ts
+++ src/migrations/1666619963285-AppleNull.ts
@@ -4,7 +4,7 @@ export class AppleNull1666619963285 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `ALTER TABLE users MODIFY apple_id varchar(100), MODIFY apple_id varchar(256)`
+            `ALTER TABLE users MODIFY apple_id varchar(100), MODIFY apple_email varchar(256)`
         )
     }
 
