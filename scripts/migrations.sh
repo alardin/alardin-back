a155ea21f88cd007c33e24c3e8eebe02385e8aa3
조병근 <airmancho@korea.ac.kr>
2022-10-26 17:02:41 +0900

    test

--- scripts/migrations.sh
+++ scripts/migrations.sh
@@ -3,5 +3,4 @@
 echo "[*] RUN Migrate"
 docker system prune -f
 cd /home/ec2-user/alardin-back-migrations
-cp ../.alardin-envs/.migration.env .env && npm install && npm run migrate:run
-cd .. && rm -rf alardin-back-migrations
+cp ../.alardin-envs/.migration.env .env && npm install && npm run migrate:run && cd .. && rm -rf alardin-back-migrations
